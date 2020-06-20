0ae4be2f5ca196e615607aa359f98218d1309f36
Emanoel Nascimento <em.nasc@outlook.com>
2020-06-19 23:45:04 -0300

    Updating gitignore Adding initial script implementation Adding pipfile

---
+++ bin_to_dec.py
@@ -0,0 +1,22 @@
+import re
+
+def convert(input: str):
+    if (not re.findall(r'\d{1,8}[0, 1]+', input)) or len(input) > 8:
+        if 1 > len(input) > 8:
+            specifics = 'Invalid input length'
+        elif not input.isnumeric():
+            specifics = 'Input is not numeric'
+        else:
+            specifics = 'Input is not binary'
+
+        print(f'Invalid input {input}: {specifics}')
+        return None
+
+    total = 0
+    current = 1
+    for digit in input[::-1]:
+        if digit == '1':
+            total += current
+
+        current *= 2
+    return total
