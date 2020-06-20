769ab976d2a323e8958105d9dbf8a13418ec090c
Emanoel Nascimento <em.nasc@outlook.com>
2020-06-19 23:52:01 -0300

    Splitting conversion and input validation

--- bin_to_dec.py
+++ bin_to_dec.py
@@ -1,6 +1,6 @@
 import re
 
-def convert(input: str):
+def __validate(input: str):
     if (not re.findall(r'\d{1,8}[0, 1]+', input)) or len(input) > 8:
         if 1 > len(input) > 8:
             specifics = 'Invalid input length'
@@ -8,15 +8,17 @@ def convert(input: str):
             specifics = 'Input is not numeric'
         else:
             specifics = 'Input is not binary'
-
         print(f'Invalid input {input}: {specifics}')
-        return None
+        return False
+    return True
 
+def convert(input: str):
+    if not __validate():
+        return None
     total = 0
     current = 1
     for digit in input[::-1]:
         if digit == '1':
             total += current
-
         current *= 2
     return total
